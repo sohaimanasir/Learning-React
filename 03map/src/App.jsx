f4e84b62df3e7e89d48987a5ad0cf988ec5a97e8
Sohaima <sohaimanasir1@gmail.com>
2025-02-24 12:07:13 +0500

    cards

--- 03map/src/App.jsx
+++ 03map/src/App.jsx
@@ -2,17 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const data =[
+  {title: "Sohaima"},
+  {title: "Anusha"},
+  {title: "Mehrab"},
+  {title: "Tuba"},
+  {title: "Shameen"}
+]
+
 
   return (
     <>
+    <div className="main">
+    {data.map((ele)=>(
   <div className="card">
-    <h1>Title</h1>
-    <h2>Sub-Title</h2>
-    <h3>Description</h3>
+    <h1>{ele.title}</h1>
+    {/* <h2>{subTitle}</h2>
+    <h3>{description}</h3> */}
   </div>
-    </>
   )
-}
+)}</div>
+</>
+)}
 
 export default App
